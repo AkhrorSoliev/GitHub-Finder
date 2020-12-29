88efafa6c61a8d872196572712cf520d4c55aade
Akhror <akhsoliev@gmail.com>
2020-12-29 09:41:05 +0500

    set http request and use sync function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import axios from 'axios'
 import './App.css'
 
-class App extends React.Component {
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  }
+
+  async componentDidMount() {
+    const res = await axios.get('http://api.github.com/users')
+
+    this.setState({ users: res.data, loading: false })
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     )
